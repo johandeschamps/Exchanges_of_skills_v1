05d39f604c563116fc10ac49de3a0459af63fc20
Johan Deschamps <johan.deschamps@fms-ea.com>
2024-11-15 09:28:24 +0100

    update small correction in code

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from .models import Skill, Slot, Visitor
 
 
 def index(request):
     return render(request, 'polls/index.html')
+
+def display_skills(request):
+    skills = Skill.objects.all()
+    return render(request, 'polls/skills.html', {'skills': skills})
+
+def display_slots(request):
+    slots = Slot.objects.all()
+    return render(request, 'polls/slots.html', {'slots': slots})
