7366dc9595d992f1d2f8a19a17d09967fd1ccae9
Johan Deschamps <johan.deschamps@fms-ea.com>
2024-11-15 15:35:00 +0100

    add possibility to add user without slot in admin

--- polls/admin.py
+++ polls/admin.py
@@ -1,8 +1,13 @@
 # polls/admin.py
-
 from django.contrib import admin
 from .models import User, Skill, Slot
 
-admin.site.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'name', 'email_address')
+    search_fields = ('first_name', 'name', 'email_address')
+    filter_horizontal = ('skills',)  # Affiche les compétences dans un widget horizontal
+    exclude = ('search_slots', 'help_slots')  # Exclut les champs de créneaux
+
+admin.site.register(User, UserAdmin)
 admin.site.register(Skill)
 admin.site.register(Slot)
